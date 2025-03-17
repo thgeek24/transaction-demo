19a06954d019fc9a8337341afab2eb1d2bfb0e31
THGeek <alex24ht@hotmail>
2025-03-17 23:54:27 +0800

    feat: add controller and transaction service

---
+++ src/main/java/com/thgeek/banking/transaction/controller/TransactionController.java
@@ -0,0 +1,19 @@
+package com.thgeek.banking.transaction.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Transaction Controller
+ *
+ * @author Tao Hong
+ * @version 1.0
+ * @since 2025/03/17 23:20
+ */
+@RestController
+@RequestMapping("/api/banking/transaction")
+public class TransactionController {
+    @GetMapping
+    public
+}
---
+++ src/main/java/com/thgeek/banking/transaction/service/TransactionService.java
@@ -0,0 +1,12 @@
+package com.thgeek.banking.transaction.service;
+
+/**
+ * Transaction Service
+ *
+ * @author Tao Hong
+ * @version 1.0
+ * @since 2025/03/17 23:22
+ */
+public interface TransactionService {
+
+}
