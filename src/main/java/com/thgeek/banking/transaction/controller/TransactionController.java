76d0d139ce23c8c47521bc4a04fc2428701bae5a
THGeek <alex24ht@hotmail>
2025-03-18 15:56:33 +0800

    feat: add CRUD endpoints for transactions

--- src/main/java/com/thgeek/banking/transaction/controller/TransactionController.java
+++ src/main/java/com/thgeek/banking/transaction/controller/TransactionController.java
@@ -1,6 +1,16 @@
 package com.thgeek.banking.transaction.controller;
 
+import com.thgeek.banking.transaction.dto.CreateTransactionReq;
+import com.thgeek.banking.transaction.dto.TransactionQuery;
+import com.thgeek.banking.transaction.dto.UpdateTransactionReq;
+import com.thgeek.banking.transaction.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +22,33 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2025/03/17 23:20
  */
 @RestController
-@RequestMapping("/api/banking/transaction")
+@RequestMapping("/api/transactions")
 public class TransactionController {
+    private final TransactionService transactionService;
+
+    @Autowired
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
     @GetMapping
-    public
+    public ResponseEntity<?> query(TransactionQuery query) {
+        return ResponseEntity.ok(transactionService.query(query));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(CreateTransactionReq req) {
+        return ResponseEntity.ok(transactionService.create(req));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, UpdateTransactionReq req) {
+        return ResponseEntity.ok(transactionService.update(id, req));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        transactionService.delete(id);
+        return ResponseEntity.ok().build();
+    }
 }
