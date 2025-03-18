f527b67dae571a0e757f94359c8a56730e5ef3aa
THGeek <alex24ht@hotmail>
2025-03-18 15:47:34 +0800

    feat: enable caching in TransactionApplication

--- src/main/java/com/thgeek/banking/transaction/TransactionApplication.java
+++ src/main/java/com/thgeek/banking/transaction/TransactionApplication.java
@@ -2,8 +2,10 @@ package com.thgeek.banking.transaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class TransactionApplication {
 
     public static void main(String[] args) {
