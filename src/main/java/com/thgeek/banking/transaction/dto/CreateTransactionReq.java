866ecd983ff44a497e4d8aed266944aa648781d9
THGeek <alex24ht@hotmail>
2025-03-18 23:24:18 +0800

    feat: add validation for transaction amount with minimum constraint

--- src/main/java/com/thgeek/banking/transaction/dto/CreateTransactionReq.java
+++ src/main/java/com/thgeek/banking/transaction/dto/CreateTransactionReq.java
@@ -1,5 +1,6 @@
 package com.thgeek.banking.transaction.dto;
 
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -27,6 +28,7 @@ public class CreateTransactionReq {
     private String trxReferenceNo;
 
     @NotNull
+    @DecimalMin("0.01")
     private BigDecimal amount;
 
     @NotBlank
